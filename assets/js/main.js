f7ad46f0c92ad23b8835cf321581b006ed7bfdd1
radabp <58158602+mbadarp@users.noreply.github.com>
2021-12-26 14:27:02 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,7 +147,7 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({
     distance: '60px',
     duration: 2800,
-    reset: true,
+    // reset: true,
 })
 
 sr.reveal(`.home__data, .home__social-link, .home__info, .discover__container, .experience__data, .experience__overlay, .place__card, .sponsor__content, .footer__data, .footer__rights`, {
